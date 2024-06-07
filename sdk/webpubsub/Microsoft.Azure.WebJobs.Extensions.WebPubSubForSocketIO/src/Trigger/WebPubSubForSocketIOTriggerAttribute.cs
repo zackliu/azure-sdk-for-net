bf98b6416689f2ebe6d9957f3c8e3b6bc4e6c9f7
Chenyang Liu <chenyl@microsoft.com>
2024-06-07 12:49:54 +0800

    More updates

--- sdk/webpubsub/Microsoft.Azure.WebJobs.Extensions.WebPubSubForSocketIO/src/Trigger/WebPubSubForSocketIOTriggerAttribute.cs
+++ sdk/webpubsub/Microsoft.Azure.WebJobs.Extensions.WebPubSubForSocketIO/src/Trigger/WebPubSubForSocketIOTriggerAttribute.cs
@@ -9,7 +9,7 @@ using Microsoft.Azure.WebJobs.Description;
 namespace Microsoft.Azure.WebJobs.Extensions.WebPubSubForSocketIO
 {
     /// <summary>
-    /// Attribute used to bind a parameter to an Azure Web PubSub when a Web PubSub request is coming.
+    /// Attribute used to bind a parameter to an Azure Web PubSub when a Web PubSub for Socket.IO request is coming.
     /// </summary>
     [AttributeUsage(AttributeTargets.Parameter)]
 #pragma warning disable CS0618 // Type or member is obsolete
@@ -18,49 +18,28 @@ namespace Microsoft.Azure.WebJobs.Extensions.WebPubSubForSocketIO
     public class WebPubSubForSocketIOTriggerAttribute : Attribute
     {
         /// <summary>
-        /// Attribute used to bind a parameter to an Azure Web PubSub, when an request is from Azure Web PubSub service.
+        /// Attribute used to bind a parameter to an Azure Web PubSub for Socket.IO, when an request is from Azure Web PubSub for Socket.IO.
         /// </summary>
         /// <param name="hub">Target hub name of the request.</param>
         /// <param name="eventType">Target event name of the request.</param>
         /// <param name="eventName">Target event type of the request.</param>
-        /// <param name="connections">Connection strings of allowed upstreams for signature checks.</param>
-        public WebPubSubForSocketIOTriggerAttribute(string hub, WebPubSubEventType eventType, string eventName, params string[] connections)
+        /// <param name="namespace">Target namespace.</param>
+        public WebPubSubForSocketIOTriggerAttribute(string hub, WebPubSubEventType eventType, string eventName, string @namespace)
         {
             Hub = hub;
             EventName = eventName;
             EventType = eventType;
-            Connections = connections;
+            Namespace = @namespace;
         }
 
         /// <summary>
-        /// Attribute used to bind a parameter to an Azure Web PubSub, when an request is from Azure Web PubSub service.
+        /// Attribute used to bind a parameter to an Azure Web PubSub for Socket.IO, when an request is from Azure Web PubSub service for Socket.IO.
         /// </summary>
         /// <param name="hub">Target hub name of the request.</param>
         /// <param name="eventType">Target event name of the request.</param>
         /// <param name="eventName">Target event type of the request.</param>
         public WebPubSubForSocketIOTriggerAttribute(string hub, WebPubSubEventType eventType, string eventName)
-            : this(hub, eventType, eventName, null)
-        {
-        }
-
-        /// <summary>
-        /// Attribute used to bind a parameter to an Azure Web PubSub, when an request is from Azure Web PubSub service.
-        /// </summary>
-        /// <param name="eventType">Target event name of the request.</param>
-        /// <param name="eventName">Target event type of the request.</param>
-        /// <param name="connections">Connection strings of allowed upstreams for signature checks.</param>
-        public WebPubSubForSocketIOTriggerAttribute(WebPubSubEventType eventType, string eventName, params string[] connections)
-            : this("", eventType, eventName, connections)
-        {
-        }
-
-        /// <summary>
-        /// Attribute used to bind a parameter to an Azure Web PubSub, when an request is from Azure Web PubSub service.
-        /// </summary>
-        /// <param name="eventType">Target event name of the request.</param>
-        /// <param name="eventName">Target event type of the request.</param>
-        public WebPubSubForSocketIOTriggerAttribute(WebPubSubEventType eventType, string eventName)
-            : this ("", eventType, eventName)
+            : this(hub, eventType, eventName, "/")
         {
         }
 
@@ -83,8 +62,9 @@ namespace Microsoft.Azure.WebJobs.Extensions.WebPubSubForSocketIO
         public WebPubSubEventType EventType { get; }
 
         /// <summary>
-        /// Allowed service upstream ConnectionString for Signature checks.
+        /// The namespace
         /// </summary>
-        public string[] Connections { get; }
+        [Required]
+        public string Namespace { get; }
     }
 }
